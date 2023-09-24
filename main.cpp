b59345e7fed3d84b23c30cf1898f63045ce5dfb6
NielsJ20 <NCJ.Jacobs@student.han.nl>
2023-09-24 11:50:36 +0200

    get contouren

--- main.cpp
+++ main.cpp
@@ -21,14 +21,66 @@ Mat processingImage(Mat image)
     return img_dilated;
 }
 
+void getContours(Mat &img_processed, Mat &image)
+{
+    string objectType;
+
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(img_processed, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    vector<vector<Point>> conPoly(contours.size());
+    vector<Rect> boundRect(contours.size());
+
+    for (size_t i = 0; i < contours.size(); i++)
+    {
+        // Approximate the contour to reduce the number of vertices
+        float epsilon = 0.03 * arcLength(contours[i], true);
+        approxPolyDP(contours[i], conPoly[i], epsilon, true);
+        
+        // Calculate the aspect ratio of the bounding rectangle
+        boundRect[i] = boundingRect(contours[i]);
+        float aspectRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+
+        // Determine the middle of the contour
+        Moments M = moments(contours[i]);
+        int posX = M.m10 / M.m00;
+        int posY = M.m01 / M.m00; 
+
+        // Determine the number of corners (vertices)
+        int numCorners = static_cast<int>(conPoly[i].size());
+
+        // Based on the characteristics, classify the shape
+        if (numCorners == 3) {
+            objectType = "triangle";
+        } else if (numCorners == 4) {
+            if (aspectRatio >= 0.95 && aspectRatio <= 1.05) {
+                objectType = "square";
+            } else {
+                objectType = "rectangle";
+            }
+        } else if (numCorners > 4 && numCorners < 6) {
+            objectType = "half-circle";
+        } else if (numCorners >= 6) {
+            objectType = "circle";
+        } 
+
+        drawContours(image, conPoly, i, Scalar(0, 0, 0), 2); 
+    }
+}
+
 int main(int argc, char** argv )
 {
     // Read an image.
     Mat image = imread("../Neon_shapes.png");
 
+    Mat img_processed = processingImage(image);
+
+    getContours(img_processed, image);
+
     // Show the image in window.
     imshow("image", image);
-    imshow("processed image", processingImage(image));
+    imshow("processed image", img_processed);
     waitKey(0);
 
     return 0;
